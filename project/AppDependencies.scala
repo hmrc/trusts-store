7f929368129638f7834aa649cbde31bb34d388fd
Adam Conder <adamdconder@gmail.com>
2019-09-25 18:41:25 +0100

    Upgrading bootstrap-play-25

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
   val compile = Seq(
     ws,
     "uk.gov.hmrc"             %% "play-reactivemongo"       % "6.2.0",
-    "uk.gov.hmrc"             %% "bootstrap-play-25"        % "3.10.0"
+    "uk.gov.hmrc"             %% "bootstrap-play-25"        % "5.1.0"
   )
 
   val test = Seq(
@@ -16,7 +16,7 @@ object AppDependencies {
     "com.typesafe.play"       %% "play-test"                % current                 % "test",
     "org.pegdown"             %  "pegdown"                  % "1.6.0"                 % "test, it",
     "uk.gov.hmrc"             %% "service-integration-test" % "0.2.0"                 % "test, it",
-    "com.github.tomakehurst"  %  "wiremock-standalone"      % "2.17.0"                % "test, it",
+    "com.github.tomakehurst"  %  "wiremock-standalone"      % "2.24.1"                % "test, it",
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "2.0.1"                 % "test, it",
     "org.mockito"             %  "mockito-core"             % "1.10.19"               % "test, it"
   )
