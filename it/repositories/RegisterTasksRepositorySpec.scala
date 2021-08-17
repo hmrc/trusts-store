cc36e3a0bbf2d36227effb48e6a2316ada94be22
darrylmurphy <darrylmurphy@users.noreply.github.com>
2021-08-17 09:48:04 +0100

    ignore tests in registertasksrepo2

---
+++ it/repositories/RegisterTasksRepositorySpec.scala
@@ -0,0 +1,92 @@
+package repositories
+
+import models.tasks.TaskStatus._
+import models.tasks.Tasks
+import org.scalatest._
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.concurrent.ScalaFutures
+import play.api.test.Helpers._
+import suite.MongoSuite
+import org.scalatest.matchers.must.Matchers
+
+import scala.concurrent.ExecutionContext.Implicits._
+
+class RegisterTasksRepositorySpec extends AnyFreeSpec with Matchers
+  with ScalaFutures with OptionValues with MongoSuite {
+
+  "a register tasks repository" - {
+
+    val internalId = "Int-328969d0-557e-4559-96ba-074d0597107e"
+    val draftId = "draftId"
+
+    "must return None when no cache exists" in {
+      running(application) {
+
+        getConnection(application).map{ connection =>
+          dropTheDatabase(connection)
+
+          val repository = application.injector.instanceOf[RegisterTasksRepository]
+
+          repository.get(internalId, draftId).futureValue mustBe None
+        }
+      }
+    }
+
+    "must set an updated Task and return one that exists for that user" ignore {
+      running(application) {
+
+        getConnection(application).map { connection =>
+          dropTheDatabase(connection)
+
+          val repository = application.injector.instanceOf[RegisterTasksRepository]
+
+          val task = Tasks()
+
+          val result = repository.set(internalId, draftId, task).futureValue
+
+          result mustBe true
+
+          repository.get(internalId, draftId).futureValue.value.task mustBe task
+
+          dropTheDatabase(connection)
+        }
+
+      }
+    }
+
+    "must reset the task list so every task is incomplete" ignore {
+      running(application) {
+
+        getConnection(application).map { connection =>
+          dropTheDatabase(connection)
+
+          val repository = application.injector.instanceOf[RegisterTasksRepository]
+
+          val tasksCompleted = Tasks(
+            trustDetails = Completed,
+            assets = Completed,
+            taxLiability = Completed,
+            trustees = Completed,
+            settlors = Completed,
+            protectors = Completed,
+            beneficiaries = Completed,
+            other = Completed
+          )
+
+          repository.set(internalId, draftId, tasksCompleted).futureValue
+
+          repository.get(internalId, draftId).futureValue.value.task mustBe tasksCompleted
+
+          val result = repository.reset(internalId, draftId).futureValue
+
+          result mustBe true
+
+          repository.get(internalId, draftId).futureValue.value.task mustBe Tasks()
+
+          dropTheDatabase(connection)
+        }
+
+      }
+    }
+  }
+}
