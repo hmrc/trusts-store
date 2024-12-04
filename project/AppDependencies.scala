3a29a06fabacac79e7a89b36fae5171b20587432
maheshhvr <175945803+maheshhvr@users.noreply.github.com>
2024-12-04 16:15:10 +0000

    DDCE-5841-Trusts Backend - trusts-store-Bootstrap-play 9 upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,8 +3,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private lazy val hmrcMongoVersion     = "1.9.0"
-  private lazy val bootstrapPlayVersion = "8.6.0"
+  private lazy val hmrcMongoVersion     = "2.3.0"
+  private lazy val bootstrapPlayVersion = "9.5.0"
 
   private val compile: Seq[ModuleID] = Seq(
     ws,
