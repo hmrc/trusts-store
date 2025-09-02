34714c7835775366524adf1d54ec90fb9b975979
maheshhvr <175945803+maheshhvr@users.noreply.github.com>
2025-09-02 14:16:02 +0100

    DDCE-7019 Fix Jackson Core Vulnerability [trusts-store] update dep

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -15,7 +15,7 @@ object AppDependencies {
   private val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
-    "org.scalatestplus" %% "scalacheck-1-17"         % "3.2.18.0"
+    "org.scalatestplus" %% "scalacheck-1-18"         % "3.2.19.0"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID]      = compile ++ test
