da3026d2d1a47cf2f1a6cd8e68732f1bdcd208ad
alisajadg191 <34246449+alisajadg191@users.noreply.github.com>
2026-04-01 16:44:24 +0100

    DDCE-8283 - Upgrade Bootstrap version and deps

--- build.sbt
+++ build.sbt
@@ -3,7 +3,7 @@ import uk.gov.hmrc.DefaultBuildSettings
 
 val appName = "trusts-store"
 
-ThisBuild / scalaVersion := "2.13.16"
+ThisBuild / scalaVersion := "2.13.18"
 ThisBuild / majorVersion := 0
 
 lazy val microservice = Project(appName, file("."))
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,8 +3,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private lazy val hmrcMongoVersion     = "2.10.0"
-  private lazy val bootstrapPlayVersion = "9.19.0"
+  private lazy val hmrcMongoVersion     = "2.12.0"
+  private lazy val bootstrapPlayVersion = "10.7.0"
 
   private val compile: Seq[ModuleID] = Seq(
     ws,
@@ -15,7 +15,7 @@ object AppDependencies {
   private val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapPlayVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
-    "org.scalatestplus" %% "scalacheck-1-18"         % "3.2.19.0"
+    "org.scalatestplus" %% "scalacheck-1-19"         % "3.2.19.0"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
